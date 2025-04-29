74e97ce7f3b1d034f37bbee719f7864d2c3e1e9d
kprimissia <kaiqueealan@gmail.com>
2025-04-29 10:55:17 -0300

    criação do aplication propietes

--- src/main/java/com/senai/cadastroendereco/entities/Endereco.java
+++ src/main/java/com/senai/cadastroendereco/entities/Endereco.java
@@ -1,12 +1,14 @@
 package com.senai.cadastroendereco.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Endereco {
 
 	@Id
